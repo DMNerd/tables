646cf01a62d8240f48e93e49aec59a22632d5b07
Adam <7889445+DMNerd@users.noreply.github.com>
2025-09-30 08:38:52 +0200

    Add Vite tsconfig paths and checker plugins

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,12 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
+import tsconfigPaths from 'vite-tsconfig-paths';
+import checker from 'vite-plugin-checker';
 
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
+  plugins: [
+    react(),
+    tsconfigPaths(),
+    checker({
+      typescript: true,
+    }),
+  ],
 });
