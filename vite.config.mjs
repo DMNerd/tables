d3373d82b1c9d7a3b64c130ea007601932ca5b53
Adam <mundane.laps.6k@icloud.com>
2025-10-01 11:37:37 +0200

    Add root

--- vite.config.mjs
+++ vite.config.mjs
@@ -2,13 +2,14 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import checker from 'vite-plugin-checker';
+import path from 'node:path';
 
 export default defineConfig({
+  root: __dirname,
+  publicDir: path.resolve(__dirname, 'public'),
   plugins: [
     react(),
     tsconfigPaths(),
-    checker({
-      typescript: true,
-    }),
+    checker({ typescript: true }),
   ],
 });
